024dbce5fce5bfa09e16c61463eae0a8985043ff
Vladosikkk <swimminger2@gmail.com>
2023-01-13 00:22:21 +0200

    lab6

--- lab6/lab6/lab6.cpp
+++ lab6/lab6/lab6.cpp
@@ -1,20 +1,31 @@
-// lab6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <windows.h>
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    struct Student
+    {
+        int id;
+        int group;
+        string name;
+        double averageMark;
+    };
+
+    Student student{ 1, 1, "¬ас€г" , 0 };
+    Student student1{ 2, 3, "ѕетро", 0 };
+
+    cout << "¬вед≥ть середн≥й бал студента " << student.name << ": ";
+    cin >> student.averageMark;
+
+    cout << "¬вед≥ть середн≥й бал студента " << student1.name << ": ";
+    cin >> student1.averageMark;
+
+
+    cout << student.name << " навчаЇтьс€ в груп≥ " << student.averageMark << endl;
+    cout << student1.name << " навчаЇтьс€ в груп≥ " << student1.averageMark << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
